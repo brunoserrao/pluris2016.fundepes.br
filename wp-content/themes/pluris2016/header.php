6b91f4296f6e78b1139f0a4fd741cdb84e001105
Bruno Serrão <brunosserrao@gmail.com>
2016-05-12 12:42:23 -0300

    Google Analytics

--- wp-content/themes/pluris2016/header.php
+++ wp-content/themes/pluris2016/header.php
@@ -16,6 +16,17 @@
 	<meta property="og:image" content="<?php echo get_template_directory_uri().'/images/pluris-2016-2.png'; ?>"/>
 
 	<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon.png" type="image/png">
+
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-77647694-1', 'auto');
+		ga('send', 'pageview');
+	</script>
+
 	<?php wp_head(); ?>
 </head>
 
