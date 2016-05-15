1d9634c9b1653a72ba6eded4d853dade9bc477b4
Bruno Serrão <brunosserrao@gmail.com>
2016-05-15 20:37:43 -0300

    Footers logo

--- wp-content/themes/pluris2016/footer.php
+++ wp-content/themes/pluris2016/footer.php
@@ -1,7 +1,25 @@
 		<div id="cnpj">
-			Secretaria Executiva: <a href="http://www.fundepes.br">Fundepes</a><br />
-			Rua do livramento, 148 - 6º Andar - Edf. Walmap Centro - Maceió/AL<br />
-			CEP: 57020-030 Fones/Fax: (82) 2122-5353<br />
+			<p><?php echo __('[:pb]Apoio[:en]Support[:es]Apoyo[:]') ?></p>
+			
+			<ul>
+				<li><img src="<?php echo get_template_directory_uri(); ?>/images/logos/fundepes.png" alt=""></li>
+				<li><img src="<?php echo get_template_directory_uri(); ?>/images/logos/prefeitura.png" alt=""></li>
+			</ul>
+
+			<p><?php echo __('[:pb]Organização[:en]Organization[:es]Organizacíon[:]') ?></p>
+
+			<ul>
+				<li><img src="<?php echo get_template_directory_uri(); ?>/images/logos/deha.png" alt=""></li>
+				<li><img src="<?php echo get_template_directory_uri(); ?>/images/logos/ufal.png" alt=""></li>
+				<li><img src="<?php echo get_template_directory_uri(); ?>/images/logos/usp.png" alt=""></li>
+				<li><img src="<?php echo get_template_directory_uri(); ?>/images/logos/universidade-do-minho.png" alt=""></li>
+			</ul>
+
+			<p>
+				Secretaria Executiva: <a href="http://www.fundepes.br" target="_blank">Fundepes</a><br />
+				Rua do livramento, 148 - 6º Andar - Edf. Walmap Centro - Maceió/AL<br />
+				CEP: 57020-030 Fones/Fax: (82) 2122-5326 / 2122-5353<br />
+			</p>
 		</div>
 	</div>
 </div>
