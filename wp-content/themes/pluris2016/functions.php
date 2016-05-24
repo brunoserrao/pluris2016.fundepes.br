8022bce8e6672061929a930b2e680e63722dba1e
Bruno Serrão <brunosserrao@gmail.com>
2016-05-24 17:38:57 -0300

    Enable pages excerpt

--- wp-content/themes/pluris2016/functions.php
+++ wp-content/themes/pluris2016/functions.php
@@ -398,3 +398,11 @@ add_filter('woocommerce_order_button_html','custom_woocommerce_order_button_html
 function custom_woocommerce_order_button_html(){
 	return '<input type="submit" class="button alt" name="woocommerce_checkout_place_order" id="place_order" value="' .__('Finish payment', 'pluris2016') . '" data-value="' .__('Finish payment', 'pluris2016') . '" />';
 }
+
+/**
+* Adicionar resumo em páginas
+*/
+add_action( 'init', 'my_add_excerpts_to_pages' );
+function my_add_excerpts_to_pages() {
+	add_post_type_support( 'page', 'excerpt' );
+}
