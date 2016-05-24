4257f48e4c212a95135c60366dfa4003c09d057f
Bruno Serrão <brunosserrao@gmail.com>
2016-05-24 17:40:01 -0300

    Bruno API - __default_fields, __merge_fields

--- wp-content/plugins/bruno-api/bruno-api.php
+++ wp-content/plugins/bruno-api/bruno-api.php
@@ -11,12 +11,24 @@
 class BrunoApi {
 	private $version;
 	private $namespace;
+	private $default_fields;
+	private $posts_per_page;
+	private $paged;
 
 	public function __construct() {
 		$this->version = 1;
+		$this->posts_per_page = 10;
+		$this->__default_fields();
+
 		add_action('rest_api_init', array($this, 'register_routes'));
 	}
 
+	/**
+	* Registrar rotas para o App Pluris
+	*
+	* @param nulll
+	* @return null
+	*/
 	public function register_routes() {
 		$this->namespace = 'api' .'/v'. $this->version ;
 
@@ -33,33 +45,58 @@ class BrunoApi {
 				'callback'  => array($this, 'noticias')
 			)
 		);
+
+		register_rest_route( $this->namespace, '/pagina/(?P<id>[\d]+)',
+			array(
+				'methods'   => 'GET',
+				'callback'  => array($this, 'pagina')
+			)
+		);
+
+		register_rest_route( $this->namespace,'/home',
+			array(
+				'methods'   => 'GET',
+				'callback'  => array($this, 'home')
+			)
+		);
 	}
 
-	public function noticias($request) {
-		$id = !empty($request['id']) ? $request['id'] : false;
-		$thumb_size = !empty($id) ? 'full' : 'thumbnail';
-		$paged = !empty($request['paged']) ? $request['paged'] : 1;
-		$posts_per_page = !empty($request['posts_per_page']) ? $request['posts_per_page'] : get_option('posts_per_page');
+	/**
+	* Requisitar conteúdo da Home
+	*
+	* @param WP_REST_Request $request
+	* @return array $result
+	*/
+	public function home(WP_REST_Request $request){
+		$noticias = $this->noticias($request);
 
-		$default_fields = array('ID','post_date','post_title','post_excerpt');
+		$request['id'] = 2;
+		$apresentacao = $this->pagina($request);
+		
+		$result = array(
+			'data' => array(
+				'noticias' 		=> $noticias,
+				'apresentacao'	=> $apresentacao
+			)
+		);
+
+		return $result;
+	}
+
+	/**
+	* Requisitar uma página por ID
+	*
+	* @param WP_REST_Request $request
+	* @return array $result
+	*/
+	public function pagina(WP_REST_Request $request){
+		$id = $request['id'];
 		$query_args = array();
 		
-		if (!empty($request['fields'])) {
-			$default_fields = array_merge($default_fields, explode(',',$request['fields']));
-		}	
-	
-		if (!empty($request['s'])) {
-			$query_args['s'] = $request['s'];
-		}
-
-		$query_args['posts_per_page'] = $posts_per_page;
-		$query_args['paged'] = $paged;
-
-		if (!empty($id)) {
-			$query_args = array();
-			array_push($default_fields,'post_content');
-			$query_args['p'] = $id;
-		}
+		$this->__merge_fields(array('post_content','post_excerpt'));
+		
+		$query_args['post_type'] = 'page';
+		$query_args['page_id'] = $id;
 
 		$posts_query = new WP_Query();
 		$query_result = $posts_query->query( $query_args );
@@ -68,9 +105,103 @@ class BrunoApi {
 			return new WP_Error( 'rest_type_invalid', __( 'Invalid resource.' ), array( 'status' => 404 ) );
 		}
 
+		$parse_result = $this->__parse_result($query_result);
+
+		$result = array(
+			'data' => $parse_result[0]
+		);
+
+		return $result;
+	}
+
+	/**
+	* Requisitar posts ou post por ID
+	*
+	* @param WP_REST_Request $request
+	* @return array $result
+	*/
+	public function noticias(WP_REST_Request $request) {
+		$id = !empty($request['id']) ? $request['id'] : false;
+		$thumb_size = !empty($id) ? 'full' : 'thumbnail';
+		$paged = !empty($request['paged']) ? $request['paged'] : 1;
+		
+		$this->posts_per_page = !empty($request['posts_per_page']) ? $request['posts_per_page'] : get_option('posts_per_page');
+		$this->paged = !empty($request['posts_per_page']) ? $request['posts_per_page'] : 1;
+		
+		if (!empty($request['fields'])) {
+			$this->__merge_fields(explode(',',$request['fields']));
+		}	
+	
+		if (!empty($request['s'])) {
+			$query_args['s'] = $request['s'];
+		}
+
+		if (!empty($id)) {
+			$query_args = array();
+			array_push($this->default_fields,'post_content');
+			$query_args['p'] = $id;
+		}
+
+		$query_args['post_type'] = 'post';
+
+		$posts_query = new WP_Query();
+		$query_result = $posts_query->query( $query_args );
+
+		if (empty($query_result)) {
+			return new WP_Error( 'rest_type_invalid', __( 'Invalid resource.' ), array( 'status' => 404 ) );
+		}
+
+		$parse_result = $this->__parse_result($query_result);
+
+		$result = array(
+			'data' => $parse_result
+		);
+
+		if (empty($id)) {
+			$total_posts = $posts_query->found_posts;
+
+			$paging = array(
+				'actual_page' => $paged,
+				'total_pages' => ceil( $total_posts / $this->posts_per_page ),
+				'total_posts' => $total_posts
+			);
+
+			$result['paging'] = $paging;
+		}
+		
+		return $result;
+	}
+
+	/**
+	* Requisitar posts ou post por ID
+	*
+	* @param null
+	* @return null
+	*/
+	private function __default_fields(){
+		$this->default_fields = array('ID','post_date','post_title','post_excerpt');
+	}
+
+	/**
+	* Requisitar posts ou post por ID
+	*
+	* @param array $fields
+	* @return none
+	*/
+	private function __merge_fields(array $fields){
+		$this->default_fields = array_merge($this->default_fields, $fields);
+	}
+
+	/**
+	* Remove campos desnecessários do $post
+	*
+	* @param WP_Object $query_result
+	* @return WP_Object $query_result
+	*/
+	private function __parse_result(array $query_result){
 		foreach ($query_result as $key => $value) {
 			foreach ($value as $key2 => $value2) {
-				if (!in_array($key2, $default_fields)) {
+				if (!in_array($key2, $this->default_fields)) {
 					unset($query_result[$key]->$key2);
 				}
 			}
@@ -82,29 +213,12 @@ class BrunoApi {
 			$thumbnail_id = get_post_thumbnail_id( $query_result[$key]->ID );
 			$thumbnail = wp_get_attachment_image_src( $thumbnail_id, $thumb_size);
 
+			$query_result[$key]->link = get_permalink( $query_result[$key]->ID );
 			$query_result[$key]->thumbnail = $thumbnail[0];
 			$query_result[$key]->post_date = get_the_date('', $query_result[$key]->ID );
 		}
 
-		$total_posts = $posts_query->found_posts;
-		$max_pages = ceil( $total_posts / $posts_per_page );
-
-
-		$result = array(
-			'data' => $query_result
-		);
-
-		if (empty($id)) {
-			$paging = array(
-				'actual_page' => $paged,
-				'total_pages' => ceil( $total_posts / $posts_per_page ),
-				'total_posts' => $total_posts
-			);
-
-			$result['paging'] = $paging;
-		}
-		
-		return $result;
+		return $query_result;
 	}
 }
 
