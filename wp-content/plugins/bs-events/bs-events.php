6a8092ede03613bb883067d8972acd54c41cfbf8
Bruno Serrão <brunosserrao@gmail.com>
2016-06-21 17:25:58 -0300

    Button to send notification

--- wp-content/plugins/bs-events/bs-events.php
+++ wp-content/plugins/bs-events/bs-events.php
@@ -11,12 +11,89 @@
 class bsEvents {
 
 	public function __construct() {
+		$this->load_plugin_textdomain();
+
+		add_action( 'admin_menu', array($this, 'bs_events_admin_menu'));
+		add_action( 'admin_init', array($this,'register_admin_custom_fields'));
+
+		add_filter('plugin_action_links_'.plugin_basename(__FILE__), array($this, 'register_link_permission'), 10, 4);
+
 		add_action( 'add_meta_boxes', array( $this, 'add_events_metaboxes' ), 30 );
 		add_action( 'save_post', array( $this, 'save_meta_boxes' ), 1, 2 );
 		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_plugin_scripts' ),1, 3 );
 
-		$this->load_plugin_textdomain();
+		add_filter('manage_bs_posts_events_posts_columns', array($this, 'add_new_bs_posts_events'), 10);
+		add_action('manage_bs_posts_events_posts_custom_column', array($this, 'add_bs_posts_events_button_notify'), 10, 2);
+
+		add_action('wp_ajax_bs_events_send_notification', array($this, 'bs_events_send_ajax_notification'));
+		add_action('wp_ajax_nopriv_bs_events_send_notification', array($this, 'bs_events_send_ajax_notification'));
+
 		$this->init();
+	}
+
+	public function bs_events_send_ajax_notification(){
+		$url = 'https://onesignal.com/api/v1/notifications';
+
+		$id = sanitize_text_field($_REQUEST['id']);
+
+		$post_query = new WP_Query();
+
+		$post = $post_query->query(array(
+			'post_type' => array('post', 'bs_posts_events'),
+			'p' => $id
+		));
+
+
+		$redirect = '';
+
+		switch ($post[0]->post_type) {
+			case 'bs_posts_events':
+				$redirect = 'app.programacao/id';
+				break;
+			case 'post':
+				$redirect = 'app.noticias/id';
+				break;
+			default:
+				break;
+		}
+
+		$qtx = new QTX_Translator();
+		
+		$args = array(
+			'method' => 'POST',
+			'sslverify' => false,
+			'headers' => array(
+				'Authorization'  => 'Basic ' . get_option('bs_events_one_signal_api_key'),
+				'Content-Type' => 'application/json'
+			),
+			'response' => array(
+				'code'    => int,
+				'message' => string
+			),
+			'body' => json_encode(array(
+				'app_id' => get_option('bs_events_one_signal_app_id'),
+				'included_segments' => array('All'),
+				'data' => array(
+					'redirect' => $redirect,
+					'id' => $id
+				),
+				'contents' => array(
+					'en' => $qtx->translate_text($post[0]->post_title, 'en'),
+					'pt' => $qtx->translate_text($post[0]->post_title, 'pb'),
+					'es' => $qtx->translate_text($post[0]->post_title, 'es')
+				),
+				'large_icon' => 'pushicon',
+				'ios_badgeType' => 'Increase',
+				'ios_badgeCount' => 1,
+			))
+		);
+
+		$response = wp_remote_request( $url, $args );
+		// print_r($response);
+		if ( is_wp_error( $response ) ) {
+			echo $response->get_error_message();
+		}
+		wp_die();
 	}
 
 	/**
@@ -32,6 +109,46 @@ class bsEvents {
 	 *
 	 */
 	public function init(){
+		$this->register_post_type();
+	}
+
+
+	/**
+	 * Register post type events
+	 *
+	 */
+	public function register_link_permission( $actions, $plugin_file, $plugin_data, $context ) {
+		array_unshift($actions, '<a href="'.menu_page_url('bspostevents', false).'">'.__('Settings').'</a>');
+		return $actions;
+	}
+
+	/**
+	 * Register Menu Settings Link
+	 *
+	 */
+	public function bs_events_admin_menu(){
+		add_options_page(
+			__('Bruno Eventos Settings', 'bs-events'),
+			__('Bruno Eventos Settings', 'bs-events'),
+			'manage_options',
+			'bspostevents',
+			array(
+				$this,
+				'settings_page'
+			)
+		);
+	}
+
+	public function register_admin_custom_fields(){
+		register_setting( 'bsevents_post_group', 'bs_events_one_signal_app_id');
+		register_setting( 'bsevents_post_group', 'bs_events_one_signal_api_key');
+	}
+
+	/**
+	 * Register post type events
+	 *
+	 */
+	public function register_post_type(){
 		register_post_type( 'bs_posts_events',
 			apply_filters( 'bs_register_post_type_event',
 				array(
@@ -70,24 +187,33 @@ class bsEvents {
 				)
 			)
 		);
+	}
 
+	/**
+	 * Add Column Notification
+	 *
+	 * @param  array $defaults
+	 * @return  array $defaults
+	 */
+	public function add_new_bs_posts_events($defaults) {
+		 $defaults['btn_notificar'] = __( 'Notification', 'bs-events' );
+		 return $defaults;
+	}
 
-		add_filter('manage_bs_posts_events_posts_columns', 'add_new_bs_posts_events', 10);
-		add_action('manage_bs_posts_events_posts_custom_column', 'ST4_columns_content_only_movies', 10, 2);
-
-		function add_new_bs_posts_events($defaults) {
-			 $defaults['btn_notificar'] = __( 'Notificar', 'bs-events' );
-			 return $defaults;
-		}
-
-		function ST4_columns_content_only_movies($column_name, $post_ID) {
-			switch ($column_name) {
-				case 'btn_notificar':
-					echo '<input class="page-title-action" type="button" value="'. __( 'Send notification', 'bs-events' ) .'" />';
-					break;
-				default:
-					break;
-			}
+	/**
+	 * Add button Send notification
+	 *
+	 * @param  string $column_name
+	 * @return null
+	 */
+	public function add_bs_posts_events_button_notify($column_name, $post_ID) {
+		$nonce = wp_create_nonce( 'bs_events_nonce_' . $post_ID );
+		switch ($column_name) {
+			case 'btn_notificar':
+				echo  '<input class="page-title-action button-notify" type="button" data-id="'.$post_ID.'" data-nonce="'.$nonce.'" value="'. __( 'Send', 'bs-events' ) .'" />';
+				break;
+			default:
+				break;
 		}
 	}
 
@@ -98,7 +224,7 @@ class bsEvents {
 	 * @param  object $post
 	 */
 	public function add_events_metaboxes(){
-		add_meta_box( 'bs_events_date_form_html', __( 'Event Date', 'bs-events' ), 'bsEvents::bs_events_date_form_html', 'bs_posts_events', 'side', 'default');
+		add_meta_box( 'bs_events_date_form_html', __( 'Event Date', 'bs-events' ), array($this, 'bs_events_date_form_html'), 'bs_posts_events', 'side', 'default');
 	}
 
 	/**
@@ -109,8 +235,7 @@ class bsEvents {
 		global $post;
 
 		// Noncename needed to verify where the data originated
-		echo '<input type="hidden" name="eventmeta_noncename" id="eventmeta_noncename" value="' . 
-		wp_create_nonce( plugin_basename(__FILE__) ) . '" />';
+		echo '<input type="hidden" name="eventmeta_noncename" id="eventmeta_noncename" value="' . wp_create_nonce( plugin_basename(__FILE__) ) . '" />';
 
 		// Get the location data if its already been entered
 		$date_start = get_post_meta($post->ID, 'date_start', true);
@@ -184,6 +309,42 @@ class bsEvents {
 	public function enqueue_plugin_scripts(){
 		wp_enqueue_script( 'bs-plugin-local-js', plugins_url('js/scripts.js', __FILE__ ), array('jquery'), false, true );
 		wp_enqueue_style(  'bs-plugin-local-css',  plugins_url('css/styles.css', __FILE__ ) );
+	}
+
+	/**
+	 * Register post type events
+	 *
+	 */
+	public function settings_page(){
+		?>
+			<div class="wrap">
+				<h2><?php echo __('Bruno Events Configuration', 'bs-events')?></h2>
+
+				<form method="post" action="options.php">
+				<?php settings_fields('bsevents_post_group'); ?>
+
+					<table class="form-table">
+						<tr valign="top">
+							<th scope="row">OneSignal App ID</th>
+							<td>
+								<input type="text" class="regular-text" name="bs_events_one_signal_app_id" value="<?php echo get_option('bs_events_one_signal_app_id') ?>"/>
+							</td>
+						</tr>
+
+						<tr valign="top">
+							<th scope="row">REST API Key</th>
+							<td>
+								<input type="text" class="regular-text" name="bs_events_one_signal_api_key" value="<?php echo get_option('bs_events_one_signal_api_key') ?>"/>
+							</td>
+						</tr>
+					</table>
+
+					<p class="submit">
+						<input type="submit" name="submit" id="submit" class="button button-primary" value="<?php _e('Save Changes');?>">
+					</p>
+				</form>
+			</div>
+		<?php	
 	}
 }
 
