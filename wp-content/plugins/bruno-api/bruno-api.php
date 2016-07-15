419d97cbf0b62238e4f098c5e1525060edd2f03f
Bruno Serrão <brunosserrao@gmail.com>
2016-07-15 03:53:03 -0300

    Login and Upload image

--- wp-content/plugins/bruno-api/bruno-api.php
+++ wp-content/plugins/bruno-api/bruno-api.php
@@ -102,9 +102,9 @@ class BrunoApi{
 	* @param WP_REST_Request $request
 	* @return array $result
 	*/
-	public function login(){
+	public function login(WP_REST_Request $request){
 		if ( !isset( $_SERVER['PHP_AUTH_USER'] ) ) {
-			return false;
+			return new WP_Error( 'rest_type_invalid', __( 'Empty Auth User' ), array( 'status' => 404 ) );
 		}
 
 		$username = $_SERVER['PHP_AUTH_USER'];
@@ -113,7 +113,7 @@ class BrunoApi{
 		$wp_authenticate = wp_authenticate( $username, $password );
 
 		if (!is_user_logged_in()) {
-			return new WP_Error( 'rest_type_invalid', __( 'Invalid resource.' ), array( 'status' => 401 ) );
+			return new WP_Error( 'rest_type_invalid', __( 'Login Fail.' ), array( 'status' => 404 ) );
 		}
 
 		$avatar = get_avatar_data($wp_authenticate->data->ID);
@@ -172,28 +172,48 @@ class BrunoApi{
 		$ids = get_post_gallery($pagina_id, false);
 
 		if (empty($ids)) {
-			return new WP_Error( 'rest_type_invalid', __( 'Invalid resource.' ), array( 'status' => 404 ) );
+			return new WP_Error( 'rest_type_invalid', __( 'Empty gallery.' ), array( 'status' => 404 ) );
 		}
 
-		$ids = explode(",", $ids['ids']);
+		$role_ids = get_users(
+			array(
+				'fields' => 'ID',
+				'orderby' => 'registered'
+			)
+		);
+
+		$media_query = new WP_Query(
+			array(
+				'post_type' => 'attachment',
+				'post_status' => 'inherit',
+				'post_mime_type' => 'image',
+				'author__in' =>  $role_ids,
+				'posts_per_page' => -1,
+				'meta_query' => array(
+					array(
+						'key' => 'image_from_gallery',
+						'value' => 1,
+						'compare' => '='
+					)
+				)
+			)
+		);
 
 		$fotos = array();
-
-		foreach ($ids as $id) {
-			$attachment = wp_prepare_attachment_for_js($id);
-
-			$sub = !empty($attachment['title']) ? $attachment['title'] : '';
+		
+		foreach ($media_query->posts as $post) {
+			$attachment = wp_prepare_attachment_for_js($post->ID);			
 
 			$foto = array(
-				'thumb' => wp_get_attachment_image_src($id, 'thumb_galeria' )[0],
-				'src' => wp_get_attachment_thumb_url($id),
-				'sub' => $sub
+				'thumb' => wp_get_attachment_image_src($post->ID, 'thumb_galeria' )[0],
+				'src' => $attachment['url'],
+				'sub' => $attachment['title']
 			);
 
 			array_push($fotos, $foto);
 		}
 
-		$wp_query = new WP_Query(array(
+		$galeria = new WP_Query(array(
 			'post_type' => 'page',
 			'page_id' => $pagina_id
 		));
@@ -201,8 +221,8 @@ class BrunoApi{
 		$result = array(
 			'data' => array(
 				'fotos' => $fotos,
-				'titulo' => $wp_query->posts[0]->post_title,
-				'texto' => strip_shortcodes($wp_query->posts[0]->post_content)
+				'titulo' => $galeria->posts[0]->post_title,
+				'texto' => strip_shortcodes($galeria->posts[0]->post_content)
 			)
 		);
 
@@ -216,42 +236,52 @@ class BrunoApi{
 	* @return array $result
 	*/
 	public function upload(WP_REST_Request $request){
-		$pagina_id = 259;
-		$ids = get_post_gallery($pagina_id, false);
+		$usuario = $this->login($request);
 
-		if (empty($ids)) {
-			return new WP_Error( 'rest_type_invalid', __( 'Invalid resource.' ), array( 'status' => 404 ) );
+		if (!is_user_logged_in()) {
+			return new WP_Error( 'rest_type_invalid', __( 'Login Fail.' ), array( 'status' => 404 ) );
 		}
 
-		$ids = explode(",", $ids['ids']);
-
-		$fotos = array();
-
-		foreach ($ids as $id) {
-			$attachment = wp_prepare_attachment_for_js($id);
-
-			$sub = !empty($attachment['title']) ? $attachment['title'] : '';
-
-			$foto = array(
-				'thumb' => wp_get_attachment_image_src($id, 'thumb_galeria' )[0],
-				'src' => wp_get_attachment_thumb_url($id),
-				'sub' => $sub
-			);
-
-			array_push($fotos, $foto);
+		if (empty($request['image'])) {
+			return new WP_Error( 'rest_type_invalid', __( 'Invalid image.' ), array( 'status' => 401 ) );
 		}
 
-		$wp_query = new WP_Query(array(
-			'post_type' => 'page',
-			'page_id' => $pagina_id
-		));
+		$upload_dir = wp_upload_dir();
+		$upload_path = str_replace( '/', DIRECTORY_SEPARATOR, $upload_dir['path'] ) . DIRECTORY_SEPARATOR;
+		$img = $request['image'];
+		$img = str_replace('data:image/jpeg;base64,', '', $img);
+		$img = str_replace(' ', '+', $img);
+		$decoded = base64_decode($img);
+		$filename = 'galeria.jpg';
+		$hashed_filename  = md5( $filename . microtime() ) . '_' . $filename;
+		$image_upload = file_put_contents( $upload_path . $hashed_filename, $decoded );
+
+		$file             = array();
+		$file['error']    = '';
+		$file['tmp_name'] = $upload_path . $hashed_filename;
+		$file['name']     = $hashed_filename;
+		$file['type']     = 'image/jpg';
+		$file['size']     = filesize( $upload_path . $hashed_filename );
+
+		$file_return      = wp_handle_sideload( $file, array( 'test_form' => false ) );
+
+		$filename = $file_return['file'];
+		$post_title = !empty($request['title']) ? $request['title'] : preg_replace('/\.[^.]+$/', '', basename($filename));
+
+		$attachment = array(
+			'post_mime_type' => $file_return['type'],
+			'post_title' => $post_title,
+			'post_content' => '',
+			'post_status' => 'inherit',
+			'guid' => $file_return['url']
+		);
+
+		$attach_id = wp_insert_attachment( $attachment, $filename );
+		$attach_data = wp_generate_attachment_metadata( $attach_id, $filename );
+		wp_update_attachment_metadata( $attach_id, $attach_data );
 
 		$result = array(
-			'data' => array(
-				'fotos' => $fotos,
-				'titulo' => $wp_query->posts[0]->post_title,
-				'texto' => strip_shortcodes($wp_query->posts[0]->post_content)
-			)
+			'data' => $attach_id
 		);
 
 		return $result;
