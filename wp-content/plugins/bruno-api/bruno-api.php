c78ecb2238fb62a1f08a16b071c6fc699b791472
Bruno Serrão <brunosserrao@gmail.com>
2016-07-15 16:42:43 -0300

    Update photo meta after upload

--- wp-content/plugins/bruno-api/bruno-api.php
+++ wp-content/plugins/bruno-api/bruno-api.php
@@ -279,6 +279,7 @@ class BrunoApi{
 		$attach_id = wp_insert_attachment( $attachment, $filename );
 		$attach_data = wp_generate_attachment_metadata( $attach_id, $filename );
 		wp_update_attachment_metadata( $attach_id, $attach_data );
+		update_post_meta( $attach_id, 'image_from_gallery', true );
 
 		$result = array(
 			'data' => $attach_id
